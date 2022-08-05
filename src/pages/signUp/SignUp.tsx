dd9a583986ae552a047accef0522a4a6734e27b8
Elvira Samadova <elvirasamadova@MacBook-Pro-Elvira.local>
2022-08-05 15:20:15 +0400

    update

--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -4,12 +4,7 @@ import "./SignUp.scss";
 const SignUp = () => {
   return (
     <div>
-<<<<<<< HEAD
-      
-      <h1 className='text-center'>Sign Up</h1>
-=======
       <h1 className="text-center">Sign Up</h1>
->>>>>>> c16abc7 (Fixed all bugs)
     </div>
   );
 };
