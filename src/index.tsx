b55b372cd2015a978e0bfb4b5ff8005ca0bb80c5
Elvira Samadova <elvirasamadova@MacBook-Pro-Elvira.local>
2022-08-11 10:50:16 +0400

    add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./i18n/i18n.ts";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
--- src/routes/NasaRouter.tsx
+++ src/routes/NasaRouter.tsx
@@ -1,23 +1,21 @@
 import { Route, Routes } from "react-router-dom";
-import React from "react";
+import About from "../pages/about/About";
+import ApodListing from "../pages/apodListing/ApodListing";
+import Contact from "../pages/contact/Contact";
+import NotFoundPage from "../pages/error-page/Error";
 import Home from "../pages/home/Home";
 import Login from "../pages/login-page/Login";
 import SignUp from "../pages/signUp/SignUp";
-import About from "../pages/about/About";
-import Contact from "../pages/contact/Contact";
-import NotFoundPage from "../pages/error-page/Error";
-import Apod from "../pages/apod/Apod";
-
 const NasaRouter = () => {
   return (
     <Routes>
+      <Route path="/apod" element={<ApodListing />} />
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="*" element={<NotFoundPage />} />
-      <Route path="/apod" element={<Apod />} />
     </Routes>
   );
 };
